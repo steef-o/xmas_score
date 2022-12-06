a1c04fc760fc454a96ef99ba6c3f70d1c614e75d
Steffen Andre Hagen <steffen.a.hagen@gmail.com>
2022-12-06 13:17:41 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+A React-vite-ts-tailwind-template.
---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+const App = () => (
+  <div className="App">
+    <p className="text-2xl">The React-Vite-TS-Tailwind-Eslint template</p>
+  </div>
+);
+
+export default App;
